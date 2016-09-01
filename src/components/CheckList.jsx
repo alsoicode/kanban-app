b264131da03e0c8f2347ebacb27752855cb38ce2
Brandon Taylor <Taylb14@labcorp.com>
2016-09-01 16:15:13 -0400

    Create checklist item
    Added form input and styling.

--- src/components/CheckList.jsx
+++ src/components/CheckList.jsx
@@ -9,7 +9,7 @@ export default class CheckList extends React.Component {
     let tasks = this.props.tasks.map((task) => (
       <li className="checklist__task">
         <input type="checkbox" defaultChecked={task.done} />
-        {task.name}
+        {task.name}{" "}
         <a href="#" className="checklist__task--remove" />
       </li>
     ));
@@ -17,6 +17,7 @@ export default class CheckList extends React.Component {
     return (
       <div className="checklist">
         <ul>{tasks}</ul>
+        <input type="text" className="checklist__task--add" placeholder="Type then hit enter to add a task" />
       </div>
     );
   }
